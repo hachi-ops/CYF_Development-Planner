d6ee0ac8a71fb35fadf95af3ec9d978753f62d4d
hachi-ops <g.helen.rog@gmail.com>
2022-12-27 04:10:56 +0000

    nav icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import homeIcon from ".././images/home-icon.png";
 
 function Navbar() {
   return (
     <>
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/inbox">Inbox</NavLink>
+        <NavLink to="/">
+          <img src={homeIcon} alt="home" />
+        </NavLink>
+        {/* <NavLink to="/inbox">Inbox</NavLink>
         <NavLink to="/feedbacks">Feedbacks</NavLink>
-        <NavLink to="/mentor-dash">Dashboard</NavLink>
+        <NavLink to="/mentor-dash">Dashboard</NavLink> */}
       </nav>
     </>
   );
