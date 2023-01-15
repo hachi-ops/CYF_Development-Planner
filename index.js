0f615c044330c4a19bd7cfb700b5379e9bc23152
DelroyGayle <delroygayle01@gmail.com>
2023-01-15 17:27:58 +0000

    one endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const pool = require("./db.js");
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,24 @@ if (process.env.NODE_ENV === "production") {
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
+});
+
+// Does the user have any plans?
+// Ordered from the newest to the oldest
+app.get("/plans/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const thePlans = await pool.query(
+      `SELECT * FROM plans 
+              WHERE username = $1
+              ORDER BY amended_timestamp DESC`,
+      [id]
+    );
+    res.json(thePlans.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server error: " + err.message);
+  }
 });
 
 app.get("*", (req, res) => {
