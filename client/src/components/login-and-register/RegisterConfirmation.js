1b08dd980475a716fb898c0191dfa98ac3010637
hachi-ops <elen20@protonmail.com>
2023-04-26 14:02:17 +0100

    change to register confirmation

--- client/src/components/login-and-register/RegisterConfirmation.js
+++ client/src/components/login-and-register/RegisterConfirmation.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function RegisterConfirmation() {
+function RegisterConfirmation({ setAuth }) {
   return (
     <>
       <section>
         <h2>You have succesfully signed up</h2>
         <h3>
-          Go to your <Link to="/dashboard">Dashboard</Link>{" "}
+          Go to your{" "}
+          <Link to="/dashboard" setAuth={setAuth}>
+            Dashboard
+          </Link>{" "}
         </h3>
       </section>
     </>
