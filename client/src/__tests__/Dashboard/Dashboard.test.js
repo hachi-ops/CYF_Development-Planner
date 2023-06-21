7fda14f1db4b93a602a5add2ad3d98e218412f34
hach-ops <elen20@protonmail.com>
2023-06-21 21:47:47 +0100

    tests started

--- client/src/__tests__/Dashboard/Dashboard.test.js
+++ client/src/__tests__/Dashboard/Dashboard.test.js
@@ -19,6 +19,8 @@ test("should render Logout component", () => {
   expect(logout).toBeInTheDocument();
 });
 
+// Dashboard Navigation
+
 // render DashboardNavigation component
 test("should render DashboardNavigation component", () => {
   render(<DashboardNavigation />);
@@ -27,14 +29,11 @@ test("should render DashboardNavigation component", () => {
 });
 
 // render icons/icons text
-describe("should render: icons on dashboard, icons captions, open link on click of the icon, open link on click of the ico caption", () => {
+describe("should render: icons on dashboard, icons captions", () => {
   test("should render icons on dashboard", () => {
     render(<DashboardNavigation />);
     const filesIcon = screen.getByAltText("files icon");
     expect(filesIcon).toBeInTheDocument();
-
-    const messagesIcon = screen.getByAltText("messages icon");
-    expect(messagesIcon).toBeInTheDocument();
 
     const accountIcon = screen.getByAltText("account icon");
     expect(accountIcon).toBeInTheDocument();
@@ -42,13 +41,75 @@ describe("should render: icons on dashboard, icons captions, open link on click 
 
   test("should render icons captions", () => {
     render(<DashboardNavigation />);
-    const filesCaption = screen.getByText("Files");
+    const filesCaption = screen.getByTitle("files");
     expect(filesCaption).toBeInTheDocument();
 
-    const messagesCaption = screen.getByText("Messages");
-    expect(messagesCaption).toBeInTheDocument();
-
-    const accountCaption = screen.getByText("Account");
+    const accountCaption = screen.getByTitle("account");
     expect(accountCaption).toBeInTheDocument();
   });
 });
+
+// show navigation buttons on click of the div with an icon and caption
+
+// on click of a button:
+// open all files
+
+// open sent files
+
+// open received files
+
+// open drafts
+
+// open new
+
+// all files:
+// render all files list
+
+// render sender name
+
+// render message title
+
+// render open button
+
+// on click of a button
+// render a message
+
+// render answer, cancel, delete buttons
+
+// render answer component on click of answer button
+
+// render delete prompt on click
+
+// return to the previous screen on click of a cancel button
+
+// return to the previous screen on click of X button
+
+// render sent files list
+
+// render open button, message title in each message entry
+
+// open sent file on click of an open button
+
+// render cancel, delete, X buttons
+
+// render received list on click of a button
+
+// render open button
+
+// render received message title
+
+// render received message sender username
+
+// render drafts list
+
+// render draft title and open button
+
+// open draft on click
+
+// render delete, send, edit buttons
+
+// render new draft(file) component on click of a button
+
+// render send, save buttons
+
+// render dropdown, send, X buttons on click of a send button
