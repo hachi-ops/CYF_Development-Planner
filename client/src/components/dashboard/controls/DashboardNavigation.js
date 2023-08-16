e73262624247e945ba472992b0e925ac7396bc30
hachi-ops <g.helen.rog@gmail.com>
2023-08-16 03:51:48 +0100

    tabs for dashboard

--- client/src/components/dashboard/controls/DashboardNavigation.js
+++ client/src/components/dashboard/controls/DashboardNavigation.js
@@ -15,6 +15,7 @@ function DashboardNavigation({ user, handleUpdate }) {
   const [messagesControlButtons, setMessagesControlButtons] = useState(false);
   const [accountControlButtons, setAccountControlButtons] = useState(false);
 
+  const [currentTab, setCurrentTab] = useState("1");
   const handleToggleFiles = () => {
     setFilesControlButtons(!filesControlButtons);
     setMessagesControlButtons(false);
@@ -33,65 +34,71 @@ function DashboardNavigation({ user, handleUpdate }) {
     setMessagesControlButtons(false);
   };
 
+  const boxes = [
+    {
+      id: 1,
+      title: "Files",
+      img: filesIcon,
+      alt: "files icon",
+      h2Style: {
+        borderBottom: filesControlButtons ? "2px solid #b55151" : "none",
+        transform: filesControlButtons && "scale(1.2)",
+      },
+      imgStyle: { transform: filesControlButtons && "scale(1.4)" },
+      toggle: handleToggleFiles,
+    },
+    {
+      id: 2,
+      title: "Messages",
+      img: messagesIcon,
+      alt: "messages icon",
+      h2Style: {
+        borderBottom: messagesControlButtons ? "2px solid #b55151" : "none",
+        transform: messagesControlButtons && "scale(1.2)",
+      },
+      imgStyle: { transform: messagesControlButtons && "scale(1.4)" },
+      toggle: handleToggleMessages,
+    },
+
+    {
+      id: 3,
+      title: "Account",
+      img: accountIcon,
+      alt: "account icon",
+      h2Style: {
+        borderBottom: accountControlButtons ? "2px solid #b55151" : "none",
+        transform: accountControlButtons && "scale(1.2)",
+      },
+      imgStyle: { transform: accountControlButtons && "scale(1.4)" },
+      toggle: handleToggleAccount,
+    },
+  ];
+
+  const handleTabClick = (e) => {
+    setCurrentTab(e.target.id);
+  };
   return (
     <>
-      <div className="buttons flex" data-testid="dashboard-navigation">
-        <div className="icon-heading flex" onClick={handleToggleFiles}>
-          <h2
-            style={{
-              borderBottom: filesControlButtons ? "2px solid #b55151" : "none",
-              transform: filesControlButtons && "scale(1.2)",
-            }}
+      <div className="buttons flex">
+        {boxes.map((item) => (
+          <div
+            key={item.title}
+            className="icon-heading flex"
+            onClick={item.toggle}
           >
-            Files
-          </h2>
-          <img
-            src={filesIcon}
-            alt="files icon"
-            className="dashboard-icon"
-            style={{ transform: filesControlButtons && "scale(1.4)" }}
-          />
-        </div>
+            <h2 style={item.h2Style}>{item.title}</h2>{" "}
+            <img
+              src={item.img}
+              alt="icon"
+              style={item.imgStyle}
+              className="dashboard-icon"
+            />
+          </div>
+        ))}
         {filesControlButtons && (
           <DraftsControls name={user.username} user={user} />
         )}
-        <div className="icon-heading flex" onClick={handleToggleMessages}>
-          <h2
-            style={{
-              borderBottom: messagesControlButtons
-                ? "2px solid #b55151"
-                : "none",
-              transform: messagesControlButtons && "scale(1.2)",
-            }}
-          >
-            Messages
-          </h2>
-          <img
-            src={messagesIcon}
-            alt="messages icon"
-            className="dashboard-icon"
-            style={{ transform: messagesControlButtons && "scale(1.4)" }}
-          />
-        </div>
         {messagesControlButtons && <MessagesControls name={user.username} />}
-        <div className="icon-heading flex" onClick={handleToggleAccount}>
-          <h2
-            style={{
-              borderBottom: accountControlButtons
-                ? "2px solid #b55151"
-                : "none",
-              transform: accountControlButtons && "scale(1.2)",
-            }}
-          >
-            Account
-          </h2>
-          <img
-            src={accountIcon}
-            alt="account icon"
-            className="dashboard-icon"
-            style={{ transform: accountControlButtons && "scale(1.4)" }}
-          />
-        </div>
         {accountControlButtons && (
           <AccountControls
             user={user}
@@ -99,6 +106,44 @@ function DashboardNavigation({ user, handleUpdate }) {
             setAccountControlButtons={setAccountControlButtons}
           />
         )}
+      </div>
+      <div className="container">
+        <div className="tabs icon-heading flex">
+          {boxes.map((tab, i) => (
+            <button
+              key={i}
+              id={tab.id}
+              disabled={currentTab === `${tab.id}`}
+              onClick={handleTabClick}
+            >
+              {tab.title}
+              <img
+                src={tab.img}
+                alt="icon"
+                style={tab.imgStyle}
+                className="dashboard-icon"
+              />
+            </button>
+          ))}
+        </div>
+        <div className="content ">
+          {boxes.map((tab, i) => (
+            <div key={i}>
+              {currentTab === `${tab.id}` && (
+                <div>
+                  <p className="title">{tab.title}</p>
+                  <img
+                    src={tab.img}
+                    alt="icon"
+                    style={tab.imgStyle}
+                    className="dashboard-icon"
+                  />
+                  <p>{tab.content}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
